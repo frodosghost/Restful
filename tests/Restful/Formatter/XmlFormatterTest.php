879e4407f925cee20eb1fc3444d2524926f7b097
James Rickard <james@frodosghost.com>
2013-04-02 17:40:51 +1100

    Added test for recursive format check from second lev

--- tests/Restful/Formatter/XmlFormatterTest.php
+++ tests/Restful/Formatter/XmlFormatterTest.php
@@ -7,25 +7,38 @@ use Restful\Data\AbstractData;
 
 class XmlFormatterTest extends \PHPUnit_Framework_TestCase
 {
-    private $testData;
-
-    public function setUp()
-    {
-        $this->testData = new TestData(array('root-foo' => array(
-            'foo' => 'bar'
-        )));
-    }
 
     public function testFormat()
     {
         $formatter = new XmlFormatter();
 
-        $formatter->addData($this->testData);
+        $testData = new TestData(array('root-foo' => array(
+            'foo' => 'bar'
+        )));
+
+        $formatter->addData($testData);
 
         $this->assertInstanceOf('Restful\Formatter\TestData', $formatter->getData());
 
         $this->assertEquals('<?xml version="1.0" encoding="UTF-8" ?><root-foo><foo>bar</foo></root-foo>', $formatter->format());
     }
+
+    public function testSecondLevelFormat()
+    {
+        $formatter = new XmlFormatter();
+
+        $testData = new TestData(array('root-foo' => array(
+            'foo' => array(
+                'foo' => 'bar'
+            )
+        )));
+
+        $formatter->addData($testData);
+
+        $this->assertInstanceOf('Restful\Formatter\TestData', $formatter->getData());
+
+        $this->assertEquals('<?xml version="1.0" encoding="UTF-8" ?><root-foo><foo><foo>bar</foo></foo></root-foo>', $formatter->format(), '->format() returns a second level of data.');
+    }   
 
     public function testContentType()
     {
